642c7fcef424752fca576ef6a82948f61f41ef3d
suvmer <suvmer@mail.ru>
2023-07-01 00:04:45 +0300

    Slider + new images

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//header bg logic
 const header = document.querySelector('.header')
 window.onscroll = function() {
     var currentScrollPos = window.scrollY;
@@ -8,10 +9,9 @@ window.onscroll = function() {
         header.style.backgroundColor = "rgba(38, 0, 255, 0)";
         header.style.boxShadow = `rgba(0, 0, 0, 0) 0px 4px 10px`;
     }
-    //console.log(currentScrollPos);
   }
 
-
+//modal img logic
 function openImg(path) {
     const modal = document.createElement("div");
     modal.classList.add("modal");
@@ -29,3 +29,35 @@ function closeImg(e) {
     e.target.removeEventListener("click", closeImg);
     document.body.removeChild(e.target);
 }
+
+//Slidebar logic
+const slidesContainer = document.getElementById("slides");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("arrow-left");
+const nextButton = document.getElementById("arrow-right");
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+nextButton.addEventListener("click", () => {
+  slidesContainer.scrollLeft += slide.clientWidth;
+});
+prevButton.addEventListener("click", () => {
+  slidesContainer.scrollLeft -= slide.clientWidth;
+});
+
+slidesContainer.addEventListener('mousedown', (e) => {
+  isDown = true;
+  startX = e.pageX - slidesContainer.offsetLeft;
+  scrollLeft = slidesContainer.scrollLeft;
+});
+slidesContainer.addEventListener('mouseleave', () => { isDown = false; });
+slidesContainer.addEventListener('mouseup', () => {  isDown = false; });
+slidesContainer.addEventListener('mousemove', (e) => {
+  if(!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slidesContainer.offsetLeft;
+  const walk = (x - startX) * 3; //scroll-fast
+  slidesContainer.scrollLeft = scrollLeft - walk;
+});
