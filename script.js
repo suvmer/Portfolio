7056509e80a8f211d056f7fb70a5cdab37bf6a53
suvmer <suvmer@mail.ru>
2023-08-05 05:15:00 +0300

    new own slider from /sliderJS repo(https://github.com/suvmver/sliderJS)

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ window.onscroll = function() {
   }
 
 //modal img logic
-function openImg(path) {
+function openImg(path, start="/images/") {
     const modal = document.createElement("div");
     modal.classList.add("modal");
     const img = document.createElement("img");
     img.classList.add("modal_img");
-    img.src = "/images/"+path;
+    img.src = start+path;
     modal.appendChild(img);
     modal.addEventListener("click", closeImg);
     document.body.appendChild(modal);
@@ -30,34 +30,104 @@ function closeImg(e) {
     document.body.removeChild(e.target);
 }
 
-//Slidebar logic
-const slidesContainer = document.getElementById("slides");
-const slide = document.querySelector(".slide");
-const prevButton = document.getElementById("arrow-left");
-const nextButton = document.getElementById("arrow-right");
+window.onload = () => {
+  var onOpen = (url) => {
+      openImg(url, "")
+      console.log("opened " + url);
+  }
 
-let isDown = false;
-let startX;
-let scrollLeft;
+  const sliderWrapper = document.querySelector(".slider");
+  const slider = document.querySelector(".slider__body");
+  const [arrowLeft, arrowRight] = document.querySelectorAll(".slider__arrow");
+  let startPos = null; //состояние на момент нажатия слайдера
+  
+  //ширина картинок внутри слайдера
+  let widths = [...slider.children].map(el => el.clientWidth);
+  sliderWrapper.style.cssText = `width: ${Math.max(...widths)}px;`;
 
-nextButton.addEventListener("click", () => {
-  slidesContainer.scrollLeft += slide.clientWidth;
-});
-prevButton.addEventListener("click", () => {
-  slidesContainer.scrollLeft -= slide.clientWidth;
-});
+  let imgCount = slider.children.length;
+  let offsetStart = slider.clientWidth/2 - slider.children[0].clientWidth/2;
+  let offsetEnd = slider.clientWidth/2 - slider.children[imgCount-1].clientWidth/2;
+  slider.children[0].style.cssText = `margin-left: ${offsetStart}px;`;
+  slider.children[imgCount-1].style.cssText = `margin-right: ${offsetEnd}px;`;
 
-slidesContainer.addEventListener('mousedown', (e) => {
-  isDown = true;
-  startX = e.pageX - slidesContainer.offsetLeft;
-  scrollLeft = slidesContainer.scrollLeft;
-});
-slidesContainer.addEventListener('mouseleave', () => { isDown = false; });
-slidesContainer.addEventListener('mouseup', () => {  isDown = false; });
-slidesContainer.addEventListener('mousemove', (e) => {
-  if(!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - slidesContainer.offsetLeft;
-  const walk = (x - startX) * 3; //scroll-fast
-  slidesContainer.scrollLeft = scrollLeft - walk;
-});
+  let startPoints = [];
+  [...slider.children].reduce((acc, el) => {
+      startPoints.push(acc + el.clientWidth/2); //относительные координаты середин слайдов
+      return acc + el.clientWidth;
+  }, offsetStart);
+
+  /**
+   * Возвращает индекс картинки, на которую попадает центр окна слайдера
+   * @returns индекс
+   */
+  function getCur() {
+      return [...slider.children].reduce((prev, el, ind) => {
+          let viewPos = slider.scrollLeft + slider.clientWidth/2;
+          return (startPoints[ind] - widths[ind]/2 <= viewPos && startPoints[ind] + widths[ind]/2 >= viewPos ? ind : prev);
+      }, 0);
+  }
+  /**
+   * Возвращает индекс соседа, в сторону которого отклонился
+   *  центр слайдера более, чем на 1/6 от ширины картинки.
+   * Если отклонение менее 1/6, возвращается текущее изображение
+   * @returns индекс
+   */
+  function getClosest(cur = getCur()) {
+      const wind = slider.scrollLeft + slider.clientWidth/2;
+      if(wind <= startPoints[cur] - slider.children[cur].clientWidth/6)
+          return Math.max(0, cur-1);
+      if(wind >= startPoints[cur] + slider.children[cur].clientWidth/6)
+          return Math.min(imgCount-1, cur+1);
+      return cur;
+  }
+  /**
+   * Выравнивает слайдер согласно текущем положению
+   * @param {number} cur Слайд до начала скролла
+   * @param {number?} dir Принудительно указать направление движения
+   */
+  const move = (cur, dir = null) => {
+      let index = getCur(); //индекс картинки, на который двигать
+      if(index === cur) //если центр попал на ту же картинку
+          index = getClosest(cur); //то может сдвига достаточно для соседа
+      if(dir) //если принудительно указано направление
+          index = cur + dir;
+      slider.scrollTo({left: startPoints[index] - slider.clientWidth/2, top: 0, behavior: "smooth"});
+  }
+
+  //startPos = [x, y, скролл слайдера на момент нажатия, наведённый слайд на момент нажатия]
+  function mouseDown(event) {
+      startPos = [event.clientX, event.clientY, slider.scrollLeft, getCur()];
+  }
+  function mouseUp(event) {
+      if(!startPos)
+          return;
+      if(dist(startPos[0], startPos[1], event.clientX, event.clientY) < 30)
+          onOpen(slider.children[startPos[3]].src);
+      move(startPos[3]);
+      startPos = null;
+  }
+  function mouseOut(event) {
+      if(!startPos)
+          return;
+      if(dist(startPos[0], startPos[1], event.clientX, event.clientY) >= 30)
+          move(startPos[3]);
+      startPos = null;
+  }
+  function dist(x1, y1, x2, y2) {
+      return Math.sqrt((x2-x1)**2 + (y2-y1)**2);
+  }
+  function mouseMove(event) {
+      event.preventDefault();
+      if(!startPos)
+          return;
+      const dx = startPos[0] - event.clientX;
+      slider.scrollTo({left: startPos[2] + 2*dx, top: 0});
+  }
+  slider.addEventListener('mousemove', mouseMove)
+  slider.addEventListener('mouseout', mouseOut)
+  slider.addEventListener('mousedown', mouseDown)
+  slider.addEventListener('mouseup', mouseUp)
+  arrowLeft.addEventListener('click', () => move(getCur(), -1))
+  arrowRight.addEventListener('click', () => move(getCur(), 1))
+}
